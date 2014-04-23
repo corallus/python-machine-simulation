738e94c93b033c42da2ba2eb445744efe79cdac6
Vincent van Bergen <v.vanbergen@gmail.com>
2014-04-23 10:33:54 +0200

    initial commit

--- situation4/tests.py
+++ situation4/tests.py
@@ -77,5 +77,55 @@ class TestMultipleParts(unittest.TestCase):
         self.assertNotEqual(self.machine.parts[0].broken, self.machine.parts[1].broken)
 
 
+class TestMachine(unittest.TestCase):
+
+    def setUp(self):
+        self.env = simpy.Environment()
+
+    def no_breakdowns(self):
+        part_specifications = [
+            Specification(self.env, "Test part 1", 1000, 0, 8, 1, 2, 4, 1000),
+            Specification(self.env, "Test part 2", 1000, 0, 8, 1, 2, 4, 1000)
+        ]
+        machine_specification = Specification(self.env, "Test machine", 5, 1, 15, 1, 3, 4, 50)
+        maintenance_men = simpy.Resource(self.env, capacity=100)
+        self.machine = PolicyO(self.env, 'Test machine 1', machine_specification, part_specifications, 10,
+                               maintenance_men)
+        self.env.run(until=1000)
+        self.assertEqual(self.machine.costs, 0)
 
 
+class TestPolicyO(unittest.TestCase):
+
+    def setUp(self):
+        self.env = simpy.Environment()
+
+    def no_breakdowns(self):
+        part_specifications = [
+            Specification(self.env, "Test part 1", 5, 0, 8, 1, 2, 4, 10),
+            Specification(self.env, "Test part 2", 5, 0, 8, 1, 2, 4, 10)
+        ]
+        machine_specification = Specification(self.env, "Test machine", 5, 1, 15, 1, 3, 4, 50)
+        maintenance_men = simpy.Resource(self.env, capacity=100)
+        self.machine = PolicyO(self.env, 'Test machine 1', machine_specification, part_specifications, 10,
+                               maintenance_men)
+
+    def test_repair(self):
+        raise NotImplementedError
+
+
+class TestPolicyA(unittest.TestCase):
+
+    def test_repair(self):
+        raise NotImplementedError
+
+
+class TestPolicyB(unittest.TestCase):
+
+    def test_repair(self):
+        raise NotImplementedError
+
+
+class TestPolicyAB(unittest.TestCase):
+    def test_repair(self):
+        raise NotImplementedError
